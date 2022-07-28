d6b567ce2f1bed7c758d9a113e369b1102ef2279
Talal <talalstudent@gmail.com>
2022-07-28 14:27:59 +0500

    v2

--- react-coding-challenge/components/Body.tsx
+++ react-coding-challenge/components/Body.tsx
@@ -2,44 +2,61 @@ import { useState, useEffect } from "react";
 import { Photo } from "../interfaces";
 import axios from "axios";
 import Image from "next/image";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { ClipLoader } from "react-spinners";
 
 const Body: React.FC = () => {
   const [data, setPhotosResponse] = useState<any>(null);
 
+  const fetchMoreData = () => {
+    axios.get("/api/unsplash").then((result) => {
+      setPhotosResponse(() => data.concat(result.data.result.response.results));
+      console.log(result);
+    });
+  };
+
   useEffect(() => {
     axios.get("/api/unsplash").then((result) => {
+      setPhotosResponse(result.data.result.response.results);
       console.log(result);
-      setPhotosResponse(result.data.result);
     });
   }, []);
 
   if (data === null) {
-    return <div>Loading...</div>;
+    return <ClipLoader />;
   } else if (data.errors) {
     return (
       <div>
         <div>{data.errors[0]}</div>
-        <div>PS: Make sure to set your access token!</div>
       </div>
     );
   } else {
     return (
       <div className="max-w-[1920px]">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-          {data.response.results.map((photo: Photo) => (
-            <div
-              key={photo.id}
-              className="h-[300px] w-[300px] bg-red-200 relative"
-            >
-              <Image
-                src={photo.urls.regular}
-                alt="image"
-                layout="fill"
-                objectFit="contain"
-              />
+        <InfiniteScroll
+          dataLength={data.length}
+          next={fetchMoreData}
+          hasMore={true}
+          loader={
+            <div className="my-3 w-full flex items-center justify-center">
+              <ClipLoader />
             </div>
-          ))}
-        </div>
+          }
+          style={{ overflowY: "hidden" }}
+        >
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
+            {data.map((photo: Photo) => (
+              <div key={photo.id} className="h-[300px] w-[300px]  relative">
+                <Image
+                  src={photo.urls.regular}
+                  alt="image"
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+            ))}
+          </div>
+        </InfiniteScroll>
       </div>
     );
   }
--- react-coding-challenge/pages/index.tsx
+++ react-coding-challenge/pages/index.tsx
@@ -4,7 +4,6 @@ import Body from "../components/Body";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-slate-100">
-      <h1 className="text-4xl bg-red-200">Hello</h1>
       <Body />
     </div>
   );
