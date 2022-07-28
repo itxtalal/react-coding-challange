322935d06b688446cbc399426a19e588a6f0d696
Talal <talalstudent@gmail.com>
2022-07-28 10:43:50 +0500

    init

---
+++ react-coding-challenge/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl bg-red-200">Hello</h1>
+    </div>
+  );
+};
+
+export default Home;
